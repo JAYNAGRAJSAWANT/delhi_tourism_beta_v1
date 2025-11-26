ab303cba4066f632022cb3a8e41053649004538a
Abhijeet Thorat <abhijeett@cdac.in>
2025-11-26 05:43:05 +0000

    Jay

--- dttdc_beta_v1/ebooking/models.py
+++ dttdc_beta_v1/ebooking/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # --- Core Models --- 
 
+# =========================DTTDC Tour Category Model===========================
 class DTTDCTourCategory(models.Model):
     category_name = models.CharField(max_length=350, unique=True)
     category_image = models.CharField(max_length=255, blank=True, null=True)
@@ -13,7 +14,9 @@ class DTTDCTourCategory(models.Model):
         
     def __str__(self):
         return self.category_name
-    
+
+
+# =========================DTTDC Tour Model===========================
 class DTTDCTour(models.Model):
     tour_name = models.CharField(max_length=200)
     tour_category = models.ForeignKey(DTTDCTourCategory,on_delete=models.PROTECT,related_name="tours")
@@ -33,8 +36,39 @@ class DTTDCTour(models.Model):
     
     class Meta:
         db_table = "dttdc_tour"
+        verbose_name = "tour"
+        verbose_name_plural = "tours"
         indexes = [
             models.Index(fields=["tour_category"], name="idx_dttdc_tour_tour_category"),
         ]
     def __str__(self):
         return self.tour_name
+
+# =========================DTTDC User Details Model===========================
+
+class DTTDCUserDetails(models.Model):
+    tour_booking_date=models.DateField(auto_now_add=False)
+    email=models.EmailField(max_length=45,unique=True)
+    name=models.CharField(max_length=45)
+    address=models.TextField(max_length=500)
+    city=models.CharField(max_length=45)
+    pincode=models.CharField(max_length=10)   #45 earlier
+    phone_number=models.CharField(max_length=15) #45 earlier
+    state=models.CharField(max_length=45)
+    country=models.CharField(max_length=45)
+    passport=models.CharField(max_length=20,null=True,blank=True)
+    number_of_adults=models.PositiveIntegerField()
+    number_of_child=models.PositiveIntegerField(null=True,blank=True)
+    tour_id=models.ForeignKey(DTTDCTour,on_delete=models.PROTECT,related_name="user_details")
+
+    class Meta:
+        db_table = "dttdc_user_details"
+        indexes = [
+            models.Index(fields=["tour_id"], name="idx_dttdc_user_deails_tour_id"),
+        ]
+    def __str__(self):
+        return self.name
+
+# =========================DTTDC User Details Model===========================
+
+    
